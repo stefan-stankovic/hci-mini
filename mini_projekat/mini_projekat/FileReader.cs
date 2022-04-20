e2798b60dcc839ae3530a520a58f8cd5e9815375
EvilPopS <strahinjapopovic.evilpops@gmail.com>
2022-04-21 00:09:46 +0200

    GUI - Podaci, combobox-evi i citac fajlova
    - Nova klasa FileReader koriscenja za citanje fajlova sa listom
    	svih kriptovaluta i market valuta
    
    - Implementirane metode za popunjavanje combobox-eva za biranje
    	zeljenih valuta
    
    - crypto/market-currencies.txt sadrze oznaku i pun naziv
    	valute/drzave u kojoj se koristi valuta, koji su odvojeni
    	separatorom ' -> '

---
+++ mini_projekat/mini_projekat/FileReader.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace mini_projekat {
+    internal class FileReader {
+        private const string EvilPopS_FILEPATH = @"D:\Projects\c#\hci-mini\mini_projekat\mini_projekat\";
+        private const string Stanke_FILEPATH = @"";
+
+        private const string CURRENT_FILEPATH = EvilPopS_FILEPATH;
+        private const string MARKET_VALS_FILEPATH = CURRENT_FILEPATH + "market-currencies.txt"; 
+        private const string CURRENCY_VALS_FILEPATH = CURRENT_FILEPATH + "crypto-currencies.txt";
+
+
+        private static string[] ReadFileLines(string filePath) {
+            using (StreamReader reader = new(filePath)) {
+                return reader.ReadToEnd().Split("\n");
+            }
+        }
+        public static string[] GetMarketComboBoxSelections() {
+            return ReadFileLines(MARKET_VALS_FILEPATH);
+        }
+
+        public static string[] GetCurrencyComboBoxSelections() {
+            return ReadFileLines(CURRENCY_VALS_FILEPATH);
+        }
+
+    }
+}
--- mini_projekat/mini_projekat/MainWindow.xaml.cs
+++ mini_projekat/mini_projekat/MainWindow.xaml.cs
@@ -1,39 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace mini_projekat
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+namespace mini_projekat {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
+            ComboBoxSetup();
+
             double[] dataX = new double[] { 1, 2, 3, 4, 5 };
             double[] dataY = new double[] { 1, 4, 9, 16, 25 };
             WpfPlot1.Plot.AddScatter(dataX, dataY);
             WpfPlot1.Refresh();
-
         }
 
+        private void ComboBoxSetup() {
+            SetUpMarketComboBox(FileReader.GetMarketComboBoxSelections());
+            SetUpCurrencyComboBox(FileReader.GetCurrencyComboBoxSelections());
+        }
+
+        private void SetUpMarketComboBox(string[] fileLines) {
+            prepareComboData(fileLines);
+/*
+            marketComboBox.ItemsSource = ListData;
+            marketComboBox.DisplayMemberPath = "Value";
+            marketComboBox.SelectedValuePath = "Id";
+*/
+        }
+
+        private void SetUpCurrencyComboBox(string[] fileLines) {
+            prepareComboData(fileLines);
+/*
+            currencyComboBox.ItemsSource = ListData;
+            currencyComboBox.DisplayMemberPath = "Value";
+            currencyComboBox.SelectedValuePath = "Id";
+*/
+        }
+
+        private List<ComboData> prepareComboData(string[] fileLines) {
+            List<ComboData> listData = new();
+            foreach (string line in fileLines)
+                listData.Add(
+                    new ComboData() {
+                        Id = line.Split(" -> ")[0],
+                        Value = line
+                    }
+                );
+            return listData;
+        }
 
         private void batn(object sender, RoutedEventArgs e)
         {
             APIController.GetCryptoData("CRYPTO_INTRADAY", "ETH", "USD", "5min");
         }
+    }
+    public class ComboData {
+        public string? Id { get; set; }
+        public string? Value { get; set; }
     }
 }
